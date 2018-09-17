238d9ed941fe51f81852ac55f08bff559a6bed94
Martin Held <dleh.nitram@gmail.com>
2018-09-17 21:54:15 +0200

    co2 test

--- co2reader.py
+++ co2reader.py
@@ -1,5 +1,6 @@
 import time
 import Adafruit_ADS1x15
+import math
 
 # Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
@@ -19,9 +20,9 @@ VOLTS_10000ppm = RAW_VALUE_10000ppm * MAX_VOLTS / MAX_VAL
 CO2_LOG_SLOPE = (math.log10(VOLTS_10000ppm) - math.log10(VOLTS_400ppm)) / (math.log10(10000) - math.log10(400))
 V400 = math.log10(VOLTS_400ppm) - CO2_LOG_SLOPE * math.log10(400)
 
-def contert_to_ppm(raw_value):
+def convert_to_ppm(raw_value):
     log_volts = math.log10(raw_value * MAX_VOLTS / MAX_VAL)
     return math.pow(10, (log_volts - V400) / CO2_LOG_SLOPE)
 
-def read_ppm:
+def read_ppm():
     return convert_to_ppm(adc.read_adc(0, gain=GAIN))
--- main.py
+++ main.py
@@ -5,19 +5,20 @@ import config
 
 from gsheet_reporter import GSheetReporter
 from bme280 import readBME280All
+from co2reader import read_ppm
 
 g = GSheetReporter(config.app['jsonFile'], config.app['sheetId'])
 s = sched.scheduler(time.time, time.sleep)
 
 def poll_sensors():
     timestamp = datetime.datetime.now().replace(microsecond=0).isoformat()    
-    temperature, pressure, humidity = readBME280All()
-    return [timestamp, temperature, pressure, humidity]
+#    temperature, pressure, humidity = readBME280All()
+    return [timestamp, read_ppm()]
 
 def report_data(sc): 
     sensor_data = poll_sensors()
     print(sensor_data[0], "Reporting Measurements", sensor_data)
-    g.insert("measurements", sensor_data, 2)
+#    g.insert("measurements", sensor_data, 2)
     
     s.enter(config.app['pollingDelay'], 1, report_data, (sc,))
 
