5574aed534c17e0606a9b2fa8aa45da2360e6358
Martin Held <dleh.nitram@gmail.com>
2018-10-01 20:44:14 +0200

    show some values

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const lite = require('sqlite3')
 const app = express()
+
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const db = new lite.Database('measurements.db')
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const sql = 'SELECT * FROM measurement ORDER BY created_at DESC LIMIT 20'
+
+app.get('/', (req, res) => {
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.send(rows);
+    })
+});
+
+app.listen(port, () => console.log(`Simple example app listening on port ${port}!`))
