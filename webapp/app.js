edf6ca74f0a602f935d3462d3680ea0c8afa52ca
Martin Held <dleh.nitram@gmail.com>
2018-10-04 22:12:02 +0200

    add plotting functions

---
+++ webapp/app.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const morgan = require('morgan')
+const lite = require('sqlite3')
+const app = express()
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+app.use(express.static('public'))
+
+const port = 8080
+
+const db = new lite.Database('measurements.db')
+
+const sql = "SELECT temperature, pressure, humidity, co2ppm, datetime(created_at,'localtime') as created_at FROM measurement ORDER BY created_at DESC LIMIT 300"
+
+app.get('/measurements', (req, res) => {
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.send(rows.reverse());
+    })
+});
+
+app.listen(port, '::', () => console.log(`VinoMon app listening on port ${port}!`))
---
+++ webapp/public/js/chart.js
@@ -0,0 +1,80 @@
+// chart colors
+var colors = ["#007bff", "#28a745", "#333333", "#c3e6cb", "#dc3545", "#6c757d"];
+
+function updatePlots() {
+  $.get("measurements", function(data, status) {
+    label = data.map(val => {
+      return val.created_at;
+    });
+
+    temperature = data.map((val, idx, arr) => {
+      return val.temperature;
+    });
+
+    humidity = data.map((val, idx, arr) => {
+      return val.humidity;
+    });
+
+    co2ppm = data.map((val, idx, arr) => {
+      return val.co2ppm;
+    });
+
+    pressure = data.map((val, idx, arr) => {
+      return val.pressure;
+    });
+    
+    $("#last").text(label[label.length - 1]);
+    $("#curr_temp").text("Temperatur: "+Math.round(temperature[0]*100)/100+" °C");
+    $("#curr_co2").text("CO2: "+Math.round(co2ppm[0]*100)/100+" ppm");
+    $("#curr_hum").text("rel. Feuchtigkeit: "+Math.round(humidity[0]*100)/100+" %");
+    $("#curr_press").text(pressure[0]);
+
+    drawChart("temperature", label, temperature, 1);
+    drawChart("humidity", label, humidity, 4);
+    drawChart("ppm", label, co2ppm, 0);
+    setTimeout(updatePlots, 30000);
+  });
+}
+
+updatePlots();
+
+function drawChart(id, labels, data, lineColor) {
+
+  var number = document.getElementById('foo');
+ 
+  var chLine = document.getElementById(id);
+  var chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        backgroundColor: "transparent",
+        borderColor: colors[lineColor],
+        borderWidth: 2,
+        pointBackgroundColor: colors[lineColor]
+      }
+    ]
+  };
+
+  if (chLine) {
+    new Chart(chLine, {
+      type: "line",
+      data: chartData,
+      options: {
+        animation: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false
+              }
+            }
+          ]
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
+}
