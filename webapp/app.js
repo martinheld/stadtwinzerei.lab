ed136f330e30ed5c031b793352911049172afaf7
Martin Held <dleh.nitram@gmail.com>
2018-10-09 11:41:15 +0200

    use pg

--- webapp/app.js
+++ webapp/app.js
@@ -1,30 +1,39 @@
-
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const morgan = require('morgan')
-const lite = require('sqlite3')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+const { Client } = require('pg');
+const app = express();
 
 // create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a'
+});
 // setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
-app.use(express.static('public'))
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.static('public'));
 
-const port = 8080
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+client.connect();
 
-const db = new lite.Database('../measurements.db')
+const port = 8080;
 
-const sql = "SELECT temperature, pressure, humidity, co2ppm, datetime(created_at,'localtime') as created_at FROM measurement ORDER BY created_at DESC LIMIT 600"
+const sql = "SELECT temperature, pressure, humidity, co2ppm, created_at::TIMESTAMP AT TIME ZONE 'UTC+2' as created_at FROM measurement ORDER BY created_at DESC LIMIT 600;";
 
 app.get('/measurements', (req, res) => {
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        res.send(rows.reverse());
-    })
+    client.query({ text: sql}, (err, resp) => {
+    if (err) {
+      console.log(err.stack);
+    } else {
+      console.log(resp.rows)
+      res.send(resp.rows.reverse());
+    }
+  });
 });
 
-app.listen(port, '::', () => console.log(`VinoMon app listening on port ${port}!`))
+app.listen(port, '::', () =>
+  console.log(`VinoMon app listening on port ${port}!`)
+);
